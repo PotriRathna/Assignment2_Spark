90a259ca188981d106222aa5fa090a786ee41b41
PotriRathna <rathnaprabhakaran@gmail.com>
2021-08-31 23:12:32 +0200

    Commit

--- src/main/scala/org/example/Logfile2.scala
+++ src/main/scala/org/example/Logfile2.scala
@@ -4,17 +4,42 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import java.text.SimpleDateFormat
 import java.util.Date
-
 object Logfile2 extends App {
-  case class LogLine(debug_level: String, timestamp: Date, download_id: Integer,
-                     retrieval_stage: String, rest: String)
-    Logger.getLogger("org").setLevel(Level.ERROR)
 
-    val spark = SparkSession.builder().master("local").appName("LogFile").getOrCreate()
+  // 5.	Which client did most HTTP requests?
+  def httprequest(rdd : RDD[LogLine]):Any = {
+    rdd.filter(_.retrieval_stage == "api_client").keyBy(_.download_id).
+      mapValues(l => 1).reduceByKey((a, b) => a + b).
+      sortBy(x => x._2,false).take(8)
+
+  }
+  // 6.	Which client did most FAILED HTTP requests? Use group_by to provide an answer.
+  def failedrequest(rdd : RDD[LogLine]):Any = {
+     rdd.filter(_.retrieval_stage == "api_client").
+      filter(_.rest.startsWith("Failed")).keyBy(_.download_id).
+      mapValues(l => 1).reduceByKey((a, b) => a + b).
+      sortBy(x => x._2,false).take(8)
+  }
+  // 7.	What is the most active hour of day?
+  def active(rdd : RDD[LogLine]):Any = {
+   rdd.keyBy(_.timestamp.getHours).
+    mapValues(l => 1).reduceByKey((a, b) => a + b).
+    sortBy(x => x._2,false).take(8)
+}
+  // 8.	What is the most active repository (hint: use messages from the ghtorrent.rb layer only)?
+  def activerepos(rdd: RDD[LogLine]):Any= {
+    val repos = rdd.filter(_.retrieval_stage == "api_client").
+      map(_.rest.split("/").slice(4, 6).mkString("/").takeWhile(_ != '?'))
+     repos.filter(_.nonEmpty).
+      map(x => (x, 1)).reduceByKey((a, b) => a + b).
+      sortBy(x => x._2,false).take(5)
+
+  }
+  def parsefile(spark:SparkSession):RDD[LogLine]={
     val dateFormat = "yyyy-MM-dd:HH:mm:ss"
     val regex = """([^\s]+), ([^\s]+)\+00:00, ghtorrent-([^\s]+) -- ([^\s]+).rb: (.*$)""".r
 
-    val logrdd = spark.sparkContext.
+    val log = spark.sparkContext.
       textFile("src/main/resources/ghtorrent-logs.txt").
       flatMap ( x => x match {
         case regex(debug_level, dateTime, downloadId, retrievalStage, rest) =>
@@ -22,42 +47,17 @@ object Logfile2 extends App {
           new Some(LogLine(debug_level, date.parse(dateTime.replace("T", ":")), downloadId.toInt, retrievalStage, rest))
         case _ => None
       })
-       httprequest(logrdd)
-  // 5.	Which client did most HTTP requests?
-   def httprequest(rdd : RDD[LogLine]):Any = {
-     val http_request = rdd.filter(_.retrieval_stage == "api_client").
-       keyBy(_.download_id).
-       mapValues(l => 1).
-       reduceByKey((a, b) => a + b).
-       sortBy(x => x._2, false).
-       take(8).toList
-     println(s" The client with most HTTP Request :  $http_request ")
-   }
-    // 6.	Which client did most FAILED HTTP requests?
-   val failed_http= logrdd.filter(_.retrieval_stage == "api_client").
-      filter(_.rest.startsWith("Failed")).
-      keyBy(_.download_id).
-      mapValues(l => 1).
-      reduceByKey((a,b) => a + b).
-      sortBy(x => x._2, false).
-      take(8).toList
-     println(s"The clients with most failed HTTP request : $failed_http")
+    log
+  }
+  case class LogLine(debug_level: String, timestamp: Date, download_id: Integer,
+                     retrieval_stage: String, rest: String)
+    Logger.getLogger("org").setLevel(Level.ERROR)
 
-    // 7.	What is the most active hour of day?
-   val active_hour= logrdd.keyBy(_.timestamp.getHours).
-      mapValues(l => 1).
-      reduceByKey((a,b) => a + b).
-      sortBy(x => x._2, false).
-      take(8).toList
-     println(s"The most active hours : $active_hour")
+    val spark = SparkSession.builder().master("local").appName("LogFile").getOrCreate()
+ val logrdd = parsefile(spark)
 
-  // 8.	What is the most active repository (hint: use messages from the ghtorrent.rb layer only)?
-  val repos = logrdd.filter(_.retrieval_stage == "api_client").
-    map(_.rest.split("/").slice(4,6).mkString("/").takeWhile(_ != '?'))
-  val active_repos = repos.
-    filter(_.nonEmpty).
-    map(x => (x, 1)).
-    reduceByKey((a,b) => a + b).
-    sortBy(x => x._2, false).take(5).toList
-  println(s"The most active repositories : $active_repos")
+  println(s"The clients with most HTTP request : ${httprequest(logrdd)}")
+  println(s"The clients with most failed HTTP request : ${failedrequest(logrdd)}")
+  println(s"The most active hours : ${active(logrdd)}")
+  println(s"The most active repositories : ${activerepos(logrdd)}")
 }
